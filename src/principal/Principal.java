cdf41f6a49853562318a04df86201eb12162b2c9
Carlos <cforteza91@gmail.com>
2026-05-09 13:37:46 -0300

    primer commit

---
+++ src/principal/Principal.java
@@ -0,0 +1,11 @@
+package principal;
+
+public class Principal {
+    static void main() {
+        //Comentario de prueba
+        System.out.println("Hola Mundo!");
+
+        System.out.print("Esto");
+        System.out.print(" es una prueba de impresión");
+    }
+}
